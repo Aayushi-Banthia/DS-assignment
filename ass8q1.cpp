a4e848766199dac4ae5650a1923ba401384c3cc3
Aayushi-Banthia <aabanthia01@gmail.com>
2025-11-29 12:05:15 +0530

    Add files via upload

---
+++ ass8q1.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node (int val){
+        data = val;
+        right = left = nullptr;
+    }
+};
+void inorder(Node*root){
+    if (root == nullptr)
+    return;
+    inorder(root->left);
+    cout<<root->data;
+    inorder(root->right);
+}
+void preorder(Node*root){
+    if (root == nullptr)
+    return;
+    cout<<root->data;
+    preorder(root->left);
+    preorder(root->right);
+}
+void postorder(Node*root){
+    if (root == nullptr)
+    return;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data;
+}
+int main(){
+    Node* root = new Node(8);
+    root->left = new Node(4);
+    root->right = new Node(10);
+    root->left->left = new Node(3);
+    root->left->right = new Node(6);
+
+    cout<<"Inorder Traversal: ";
+    inorder(root);
+    cout<<endl;
+
+    cout<<"Preorder Traversal: ";
+    preorder(root);
+    cout<<endl;
+
+    cout<<"Postorder Traversal: ";
+    postorder(root);
+    cout<<endl;
+
+    return 0;
+}
